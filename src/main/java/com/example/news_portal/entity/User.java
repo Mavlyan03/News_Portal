379dfe026c8d66efddc07f0266a16883b6db35d2
User <mavlyan.sadirov@gmail.com>
2022-12-25 22:33:28 +0600

    News Portal Project

---
+++ src/main/java/com/example/news_portal/entity/User.java
@@ -0,0 +1,22 @@
+package com.example.news_portal.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @SequenceGenerator(name = "user_seq",sequenceName = "user_seq",allocationSize = 1,initialValue = 10)
+    @GeneratedValue(generator = "user_seq",strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String surname;
+    private String nickname;
+    private String photo;
+}
