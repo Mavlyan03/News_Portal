8f6d6df2cc4225927f3ac628cfdf17ba15cc7e04
User <mavlyan.sadirov@gmail.com>
2022-12-26 21:52:10 +0600

    Registration

--- src/main/java/com/example/news_portal/api/AuthApi.java
+++ src/main/java/com/example/news_portal/api/AuthApi.java
@@ -1,4 +1,26 @@
 package com.example.news_portal.api;
 
+import com.example.news_portal.dto.request.SignUpRequest;
+import com.example.news_portal.dto.response.AuthResponse;
+import com.example.news_portal.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/auth")
+@CrossOrigin(origins = "*",maxAge = 3600)
+@Tag(name = "Auth Api",description = "Sign Up and Sign in")
 public class AuthApi {
+
+    private final UserService userService;
+
+    @PostMapping("/sing_up")
+    @Operation(summary = "Sign Up",
+            description = "User can sign up")
+    public AuthResponse signUp(@RequestBody SignUpRequest signUpRequest) {
+        return userService.registration(signUpRequest);
+    }
 }
