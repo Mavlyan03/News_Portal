28a822c86102c81439f3bbd0c2cc1c46db359cbc
User <mavlyan.sadirov@gmail.com>
2022-12-26 16:15:10 +0600

    Registration

--- src/main/java/com/example/news_portal/security/WebAppSecurity.java
+++ src/main/java/com/example/news_portal/security/WebAppSecurity.java
@@ -1,4 +1,23 @@
 package com.example.news_portal.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
 public class WebAppSecurity {
+
+//    private final JwtTokenVerifier verifier;
+//
+//    public WebAppSecurity(JwtTokenVerifier verifier) {
+//        this.verifier = verifier;
+//    }
+//
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
