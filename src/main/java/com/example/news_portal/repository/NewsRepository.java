28a243aec9165d0362ea242e93cd38daa162ec0d
User <mavlyan.sadirov@gmail.com>
2022-12-27 17:51:38 +0600

    News CRUD

--- src/main/java/com/example/news_portal/repository/NewsRepository.java
+++ src/main/java/com/example/news_portal/repository/NewsRepository.java
@@ -1,7 +1,24 @@
 package com.example.news_portal.repository;
 
+import com.example.news_portal.dto.response.NewsResponse;
 import com.example.news_portal.entity.News;
+import com.example.news_portal.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
+
+    @Query("select new com.example.news_portal.dto.response.NewsResponse(" +
+            "n.id," +
+            "n.header," +
+            "n.shortDescription," +
+            "n.textNews," +
+            "n.news_cover," +
+            "n.publicationDate) from News n where n.id = ?1")
+    NewsResponse getNews(Long id);
+
+    @Query("select User from News n where n.publisher.id = ?1")
+    Optional<User> getUserByNewsId(Long id);
 }
--- src/main/java/com/example/news_portal/service/NewsService.java
+++ src/main/java/com/example/news_portal/service/NewsService.java
@@ -1,5 +1,10 @@
 package com.example.news_portal.service;
 
+import com.example.news_portal.dto.request.NewsRequest;
+import com.example.news_portal.dto.response.NewsResponse;
+import com.example.news_portal.entity.News;
+import com.example.news_portal.entity.User;
+import com.example.news_portal.exception.NotFoundException;
 import com.example.news_portal.repository.NewsRepository;
 import com.example.news_portal.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,4 +16,22 @@ public class NewsService {
 
     private final NewsRepository newsRepository;
     private final UserRepository userRepository;
+
+    public NewsResponse save(NewsRequest newsRequest) {
+        News news = new News(newsRequest);
+        User publisher = userRepository.findById(newsRequest.getPublisherId())
+                .orElseThrow(() -> new NotFoundException("Publisher not found"));
+        publisher.getMyPublications().add(news);
+        news.setPublisher(publisher);
+        newsRepository.save(news);
+        return newsRepository.getNews(news.getId());
+    }
+
+    public NewsResponse delete(Long id) {
+        News news = newsRepository.findById(id).orElseThrow(
+                () -> new NotFoundException("News not found"));
+        User user = newsRepository.getUserByNewsId(news.getPublisher().getId())
+                .orElseThrow(() -> new NotFoundException("User not found"));
+        return null;
+    }
 }
