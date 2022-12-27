342278766772385636811ceb9e670903d96ec52a
User <mavlyan.sadirov@gmail.com>
2022-12-27 09:34:18 +0600

    Login

--- src/main/java/com/example/news_portal/exception/BadCredentialsException.java
+++ src/main/java/com/example/news_portal/exception/BadCredentialsException.java
@@ -1,14 +1,13 @@
 package com.example.news_portal.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import javax.security.sasl.AuthenticationException;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class BadCredentialsException extends AuthenticationException {
 
-    public BadCredentialsException(String detail) {
-        super(detail);
+    public BadCredentialsException(String msg) {
+        super(msg);
     }
 }
