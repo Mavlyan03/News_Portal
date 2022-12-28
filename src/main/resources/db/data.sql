f71e51249b4fcc65e2269e171339932804a738a9
User <mavlyan.sadirov@gmail.com>
2022-12-28 19:21:48 +0600

    Data init

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users(id, name, surname, nickname, password, photo, role)
+VALUES (1, 'Mavlyan','Sadirov','Mav','mav07','mav.com','USER'),
+       (2,'Islam','Ashirov','Islam','islam','twitter.com','USER');
+
+INSERT INTO news(id, header,news_cover,short_description,text_news,publication_date,category,publisher_id)
+VALUES (1,'World Championship','cw.com','About world of championship','Argentina won championship','2022/12/31','SPORT',1),
+       (2,'UFC','ufc.com','Mahachev champion','Mahachev against Oliveira','2022/12/21','ART',1);
+
