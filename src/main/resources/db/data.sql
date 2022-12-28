0423a884737f60d36c63954e0835540bcc899d8c
User <mavlyan.sadirov@gmail.com>
2022-12-28 19:32:54 +0600

    Data init

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO users(id, name, surname, nickname, password, photo, role)
-VALUES (1, 'Mavlyan','Sadirov','Mav','mav07','mav.com','USER'),
-       (2,'Islam','Ashirov','Islam','islam','twitter.com','USER');
+VALUES (1, 'Mavlyan','Sadirov','Mav','$2a$12$5f6qb7Tz.vXoMQpw4rMAFuaM8zfXGTRsm1xoCGb6TXzWYzeJoKmaK','mav.com','USER'),
+       (2,'Islam','Ashirov','Islam','$2a$12$.L5MDTyqWb7mxqvLhl4hsuQYUTyNCb.ZqPVAf1EIEY3VF3FO2H0O2','twitter.com','USER');
 
 INSERT INTO news(id, header,news_cover,short_description,text_news,publication_date,category,publisher_id)
 VALUES (1,'World Championship','cw.com','About world of championship','Argentina won championship','2022/12/31','SPORT',1),
