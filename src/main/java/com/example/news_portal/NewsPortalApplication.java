0998dfe528ee22f009c1bf1db6f8f047e6d000f6
User <mavlyan.sadirov@gmail.com>
2022-12-30 14:45:09 +0600

    Fixed some bugs

--- src/main/java/com/example/news_portal/NewsPortalApplication.java
+++ src/main/java/com/example/news_portal/NewsPortalApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @SpringBootApplication
@@ -12,25 +13,12 @@ public class NewsPortalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NewsPortalApplication.class, args);
-        log.info("Welcome collegues, project name is News Portal");
+        log.info("Welcome colleagues, project name is News Portal");
         System.out.println("Welcome colleagues, project name is News Portal");
     }
 
+    @GetMapping("/")
+    public String greetingPage() {
+        return "welcome";
+    }
 }
-
-//@Controller
-//@SpringBootApplication
-//@Slf4j
-//public class PeaksoftlmsB6Application {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(PeaksoftlmsB6Application.class, args);
-//		log.info("Welcome colleagues, project name is Peaksoft-LMS!");
-//		System.out.println("Welcome colleagues, project name is Peaksoft-LMS!");
-//	}
-//
-//	@GetMapping("/")
-//	public String greetingPage(){
-//		return "welcome";
-//	}
-//}
