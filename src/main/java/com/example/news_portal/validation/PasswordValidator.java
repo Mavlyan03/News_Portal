5e047caf58bd1ab99c90f5010acedabd113be958
User <mavlyan.sadirov@gmail.com>
2022-12-30 09:28:59 +0600

    Validation for password

---
+++ src/main/java/com/example/news_portal/validation/PasswordValidator.java
@@ -0,0 +1,17 @@
+package com.example.news_portal.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordValidator implements ConstraintValidator<PasswordValid,String> {
+
+    @Override
+    public boolean isValid(String password,
+                           ConstraintValidatorContext constraintValidatorContext) {
+        if(password.length() > 5) {
+            return password.matches("^[a-zA-Z0-9]{5,14}$");
+        } else {
+            return false;
+        }
+    }
+}
