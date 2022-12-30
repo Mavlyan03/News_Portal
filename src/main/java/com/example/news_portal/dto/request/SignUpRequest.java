765df3708765b3a19ed671f3bbf2291598b3ebf7
User <mavlyan.sadirov@gmail.com>
2022-12-30 09:29:45 +0600

    Validation for password

--- src/main/java/com/example/news_portal/dto/request/SignUpRequest.java
+++ src/main/java/com/example/news_portal/dto/request/SignUpRequest.java
@@ -1,5 +1,6 @@
 package com.example.news_portal.dto.request;
 
+import com.example.news_portal.validation.PasswordValid;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +10,7 @@ public class SignUpRequest {
     private String name;
     private String surname;
     private String nickname;
+    @PasswordValid
     private String password;
     private String resetPassword;
 }
