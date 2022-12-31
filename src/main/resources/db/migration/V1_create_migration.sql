5fbd9a38f16202426da6a061a7f4e22ff17de6f6
User <mavlyan.sadirov@gmail.com>
2022-12-31 19:54:24 +0600

    Add migration

---
+++ src/main/resources/db/migration/V1_create_migration.sql
@@ -0,0 +1,6 @@
+drop table if exists users;
+drop table if exists comments;
+drop table if exists news;
+create sequence if not exists user_seq;
+create sequence if not exists news_seq;
+create sequence if not exists comment_seq;
